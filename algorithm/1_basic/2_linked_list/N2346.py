6abb03416f62f7580c6acf4b834505cff6371085
Ting-Kim <taeho916@gmail.com>
2020-08-15 01:30:44 +0900

    Create N2346.py

---
+++ algorithm/1_basic/2_linked_list/N2346.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2346
+# 파이썬 List가 Linked List 기능 모두 가지고 있다길래 next랑 prev,remove 쓰려는데 안된다..
+
+
+n = 10
+inputList = [1, 2, 3, 4, 5]
+inputIter = iter(inputList)
+# print(inputList)
+index = 0
+data = inputList[index]
+
+for i in inputList:
+    if data > 0:
+        for item in range(data):
+
+            print(inputList.
+
+    else:
+        pass
